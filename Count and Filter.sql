f4a638220544e07f5a8a9d850e5b86f8ba4fd848
Shashank Sundar <shashanks0519@tamu.edu>
2024-06-17 18:07:29 -0500

    Create Count and Filter.sql

---
+++ Count and Filter.sql
@@ -0,0 +1,5 @@
+select name from students where points=(SELECT MAX(points) from students);
+select AVG(points) from students;
+select COUNT(Name) from students where points=500;
+select Name from students WHERE Name GLOB '*s*';
+select name from students ORDER BY points DESC;
